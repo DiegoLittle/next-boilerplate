7276ee46ebb1e111f53491b424ff45fdb3e3ae39
Diego Little <diegochelittle@gmail.com>
2023-10-06 10:46:19 -0500

    Updated packages and added configurable port to server.cjs

--- server.cjs
+++ server.cjs
@@ -5,39 +5,47 @@ var http = require('http');
 const dev = process.env.NODE_ENV !== 'production'
 
 
-// function getPort(port) {
-//     while (true) {
-//         try {
-//             console.log('port: ' + port);
+function getPort(port) {
+    // console.log('process.argv: ' + process.argv[2]);
+    if(process.argv[2]){
+        return process.argv[2];
+    }else{
+        return 3000
+    }
+    // if(process.argv.find((arg) => arg === '--port')){
+    //     return process.argv[process.argv.indexOf('--port') + 1];
+    // }
+    // while (true) {
+    //     try {
+    //         console.log('port: ' + port);
 
-//             var server = http.createServer(function (req, res) {
-//                 res.end('test');
-//             });
-//             try {
-//                 server.listen(3000).on('error', function (e) {
-//                     console.log('port: ' + port + ' is already in use');
-//                     port++;
-//                     return
-//                 });
-//                 server.on('listening', function () {
-//                     server.close();
-//                     return port;
-//                 });
-//             } catch (error) {
-//                 console.log('port: ' + port + ' is already in use');
-//                 port++;
-//             }
-//             port++;
-//         }
-//         catch (err) {
-//             console.log('port: ' + port + ' is already in use');
-//             port++;
-//         }
-//     }
-// }
+    //         var server = http.createServer(function (req, res) {
+    //             res.end('test');
+    //         });
+    //         try {
+    //             server.listen(3000).on('error', function (e) {
+    //                 console.log('port: ' + port + ' is already in use');
+    //                 port++;
+    //                 return
+    //             });
+    //             server.on('listening', function () {
+    //                 server.close();
+    //                 return port;
+    //             });
+    //         } catch (error) {
+    //             console.log('port: ' + port + ' is already in use');
+    //             port++;
+    //         }
+    //         port++;
+    //     }
+    //     catch (err) {
+    //         console.log('port: ' + port + ' is already in use');
+    //         port++;
+    //     }
+    // }
+}
 
-// let port = getPort(3000);
-
+let port = getPort();
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
@@ -60,9 +68,9 @@ app.prepare()
             return handle(req, res)
         })
 
-        server.listen(3005, (err) => {
+        server.listen(port, (err) => {
             if (err) throw err
-            console.log('> Ready on http://localhost:3000')
+            console.log('> Ready on http://localhost:'+port)
         })
     })
     .catch((ex) => {
